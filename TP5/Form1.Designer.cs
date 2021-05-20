01f6c300e83f2df5b5f57ff92e629eebb23e4a7a
gmfrancisco99 <gfranciscom99@gmail.com>
2021-05-20 13:01:26 -0300

    Primeros planteos de clases del proyecto, incluido el VectorEstado.

--- TP5/Form1.Designer.cs
+++ TP5/Form1.Designer.cs
@@ -28,13 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.lblN = new System.Windows.Forms.Label();
+            this.lblSimulacionMostrar = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblDesde = new System.Windows.Forms.Label();
+            this.txtDesde = new System.Windows.Forms.TextBox();
+            this.lblHasta = new System.Windows.Forms.Label();
+            this.txtHasta = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(101, 206);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(291, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(327, 107);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 1;
+            // 
+            // lblN
+            // 
+            this.lblN.AutoSize = true;
+            this.lblN.Location = new System.Drawing.Point(196, 110);
+            this.lblN.Name = "lblN";
+            this.lblN.Size = new System.Drawing.Size(125, 13);
+            this.lblN.TabIndex = 2;
+            this.lblN.Text = "Número de simulaciones:";
+            // 
+            // lblSimulacionMostrar
+            // 
+            this.lblSimulacionMostrar.AutoSize = true;
+            this.lblSimulacionMostrar.Location = new System.Drawing.Point(161, 136);
+            this.lblSimulacionMostrar.Name = "lblSimulacionMostrar";
+            this.lblSimulacionMostrar.Size = new System.Drawing.Size(160, 13);
+            this.lblSimulacionMostrar.TabIndex = 4;
+            this.lblSimulacionMostrar.Text = "Número de simulación a mostrar:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(326, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblDesde
+            // 
+            this.lblDesde.AutoSize = true;
+            this.lblDesde.Location = new System.Drawing.Point(278, 162);
+            this.lblDesde.Name = "lblDesde";
+            this.lblDesde.Size = new System.Drawing.Size(42, 13);
+            this.lblDesde.TabIndex = 7;
+            this.lblDesde.Text = "Mostrar";
+            // 
+            // txtDesde
+            // 
+            this.txtDesde.Location = new System.Drawing.Point(326, 159);
+            this.txtDesde.Name = "txtDesde";
+            this.txtDesde.Size = new System.Drawing.Size(100, 20);
+            this.txtDesde.TabIndex = 6;
+            // 
+            // lblHasta
+            // 
+            this.lblHasta.AutoSize = true;
+            this.lblHasta.Location = new System.Drawing.Point(432, 162);
+            this.lblHasta.Name = "lblHasta";
+            this.lblHasta.Size = new System.Drawing.Size(102, 13);
+            this.lblHasta.TabIndex = 9;
+            this.lblHasta.Text = "filas a partir de la fila";
+            // 
+            // txtHasta
+            // 
+            this.txtHasta.Location = new System.Drawing.Point(540, 159);
+            this.txtHasta.Name = "txtHasta";
+            this.txtHasta.Size = new System.Drawing.Size(100, 20);
+            this.txtHasta.TabIndex = 8;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(408, 206);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(291, 150);
+            this.dataGridView2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 393);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.lblHasta);
+            this.Controls.Add(this.txtHasta);
+            this.Controls.Add(this.lblDesde);
+            this.Controls.Add(this.txtDesde);
+            this.Controls.Add(this.lblSimulacionMostrar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblN);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label lblN;
+        private System.Windows.Forms.Label lblSimulacionMostrar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblDesde;
+        private System.Windows.Forms.TextBox txtDesde;
+        private System.Windows.Forms.Label lblHasta;
+        private System.Windows.Forms.TextBox txtHasta;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
---
+++ TP5/VectorEstado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5
+{
+    public class VectorEstado
+    {
+        private String evento;
+        private double reloj;
+        private double rndLlegadaCompra;
+        private double llegadaCompra;
+        private double proximaLlegadaCompra;
+        private double rndNumeroCompra;
+        private int numeroCompra;
+        private double rndLlegadaEntrada;
+        private double llegadaEntrada;
+        private double proximaLlegadaEntrada;
+        private double rndNumeroEntrada;
+        private int numeroEntrada;
+        private Boletero boletero;
+        private double rndFinCompra;
+        private double finCompra;
+        private double proximoFinCompra;
+        private Queue<Cliente> boleteria;
+        private double rndFinEntrada;
+        private double finEntrada;
+        private double proximoFinEntrada;
+        private int entrada_sala;
+        private int capacidadSala;
+
+
+        
+    }
+}
